c0f44338c3a32611efb12c624f63adc348111785
Rinkesh Modi <rinkeshmodi@Rinkeshs-MacBook-Pro.local>
2026-08-31 20:27:40 +0530

    Complete Module 2 Question 1 API Fetching solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Module2Q1 from './components/Module2Q1'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-950 text-slate-100 flex flex-col items-center justify-center p-6 font-sans">
+    <div className="">
       <Module2Q1 />
     </div>
   )
